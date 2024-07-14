369938ba991b6bfb322dd9f5ab5b56b7d1346230
Ashar581 <asharshahab@gmail.com>
2024-07-15 00:54:17 +0530

    login updated

--- public_html/auth.js
+++ public_html/auth.js
@@ -9,7 +9,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         password: pass
     };
 
-    fetch('/login-user-endpoint', { // Replace with your actual endpoint
+    fetch('http://localhost:8080/api/auth/signin', { // Replace with your actual endpoint
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -18,9 +18,33 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     })
     .then(response => response.json())
     .then(data => {
+        if(data.status === false){
+            showPopup(data.message,type = 'error');
+        }
+        else{
+            showPopup(data.message,type = 'success');
+        }
         console.log('Success:', data);
     })
     .catch((error) => {
         console.error('Error:', error);
     });
 });
+
+//here are some pop up changes...check css for it as well..
+function showPopup(message, type) {
+    const popup = document.getElementById('popup');
+    const popupMessage = document.getElementById('popupMessage');
+    popupMessage.textContent = message;
+
+    if (type === 'error') {
+        popup.style.backgroundColor = 'red';
+    } else if (type === 'success'){
+        popup.style.backgroundColor = 'green';
+    }
+    popup.style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
