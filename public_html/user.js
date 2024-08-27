657d0eda41a08bd0d144b052e10203e205bbf747
Ashar581 <asharshahab@gmail.com>
2024-08-27 16:12:30 +0530

    logging out redirects you to home page

--- public_html/user.js
+++ public_html/user.js
@@ -163,7 +163,7 @@ function closeLogoutConfirmation() {
 function logout() {
     showLoadingBar();
     localStorage.clear();
-    window.location.href = "login.html";
+    window.location.href = "home.html";
 }
 
 // Optional: Close the modal when clicking outside of it
